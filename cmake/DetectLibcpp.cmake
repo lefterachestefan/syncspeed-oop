0ed0e035c87cd84441a30e45e35c7c115b706ecc
Lefterache Ștefan <lefterache.stefan.8@gmail.com>
2026-03-23 17:12:52 +0200

    fix wip

--- cmake/DetectLibcpp.cmake
+++ cmake/DetectLibcpp.cmake
@@ -1,9 +1,13 @@
 function(detect_libcpp)
     set(OLD_CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -std=c++23")
     set(TEST_STDLIB_SRC [=[
     #include <iostream>
-    int main() {}
+    #include <expected>
+    int main() {
+        std::expected<int, int> e = 1;
+        return 0;
+    }
     ]=])
     try_compile(HAS_LIBCPP SOURCE_FROM_CONTENT test_stdlib.cpp ${TEST_STDLIB_SRC})
     set(CMAKE_CXX_FLAGS ${OLD_CMAKE_CXX_FLAGS})
