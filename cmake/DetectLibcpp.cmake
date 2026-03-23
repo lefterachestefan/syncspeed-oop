ea29f8f9bd73e17053128399613dfbcf4f34305c
Lefterache Ștefan <lefterache.stefan.8@gmail.com>
2026-03-23 17:19:58 +0200

    asdf

--- cmake/DetectLibcpp.cmake
+++ cmake/DetectLibcpp.cmake
@@ -1,15 +1,17 @@
+include(CheckCXXSourceCompiles)
+
 function(detect_libcpp)
-    set(OLD_CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -std=c++23")
-    set(TEST_STDLIB_SRC [=[
-    #include <iostream>
-    #include <expected>
-    int main() {
-        std::expected<int, int> e = 1;
-        return 0;
-    }
-    ]=])
-    try_compile(HAS_LIBCPP SOURCE_FROM_CONTENT test_stdlib.cpp ${TEST_STDLIB_SRC})
-    set(CMAKE_CXX_FLAGS ${OLD_CMAKE_CXX_FLAGS})
-    unset(OLD_CMAKE_CXX_FLAGS)
+    set(OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
+    set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -stdlib=libc++ -std=c++23")
+    
+    check_cxx_source_compiles("
+        #include <iostream>
+        #include <expected>
+        int main() {
+            std::expected<int, int> e = 1;
+            return 0;
+        }
+    " HAS_LIBCPP)
+    
+    set(CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
 endfunction()
